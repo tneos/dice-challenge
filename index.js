8fb17fad64fa465ba540c5768ca4e9326599fc68
tneos <45819936+tneos@users.noreply.github.com>
2020-10-13 19:06:41 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,31 +1,23 @@
+var randomNumber1 = Math.random();
+var randomNumber2 = Math.random();
 
-    
+randomNumber1 = Math.floor(randomNumber1 * 6) + 1;
+randomNumber2 = Math.floor(randomNumber2 * 6) + 1;
+var images = ["images/dice1.png", "images/dice2.png", "images/dice3.png", "images/dice4.png", "images/dice5.png", "images/dice6.png"];
 
-    function trigger() {
-       
+document.querySelector(".img1").setAttribute("src", images[randomNumber1 - 1]);
 
-        var input = document.querySelector('#input').value;
-    var totalAmount = 0;
-    var spin = 0;
-    console.log(input);
-    var randomNumber = Math.floor(Math.random() * 6) + 1;
-    var attr = document.querySelector('.img1');
-    attr.src = "images/dice" + randomNumber + ".png"
-    for(var i = 0; i >= 50; i++) {
-        if(randomNumber % 2 == 0) {
-            totalAmount = parseInt(input) + 1;
-            spin++;
-         }
-         else {
-             totalAmount = parseInt(input) - 0.5;
-             spin++;
-         }
-    }
-   
+document.querySelector(".img2").setAttribute("src", images[randomNumber2 - 1]);
 
-    document.querySelector('.display').innerHTML = "Total " + totalAmount;
-    document.querySelector('.spin').innerHTML = "Attempts " + spin;
-    
+//Display the winner 
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "Player 1 Wins!"
+} else if (randomNumber1 < randomNumber2) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins!"
+} else {
+  document.querySelector("h1").innerHTML = "Draw!"
 }
 
 
+console.log(randomNumber1);
+console.log(randomNumber2);
